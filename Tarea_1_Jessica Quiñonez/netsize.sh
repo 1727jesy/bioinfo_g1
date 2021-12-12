24c8eca055e55b8daa8f0b29dc47f87178620499
1727jesy <95721069+1727jesy@users.noreply.github.com>
2021-12-11 21:07:16 -0500

    Update netsize.sh
    numero de columnas

--- Tarea_1_Jessica Quiñonez/netsize.sh
+++ Tarea_1_Jessica Quiñonez/netsize.sh
@@ -1,9 +1,11 @@
-Ejercicio  1.10.3
+Ejercicio  1.10.3 
 Proceso
 el directorio
 CSB/unix/data/Saavedra2013
 -ls enlista los directorios y archivos
 - Seleccionamos  n10.txt 
-- Ingresamos el codigo
+- Ingresamos el comando
 echo "Numero de filas:" ; wc -l n10.txt ; echo "Numero de columnas:" ; head -n 1 n10.txt | tr -d ' ' | tr -d '\n' | wc -c
-#comando
+# comando de columnas y filas
+colums  ( plantas)
+Numero de columnas en n10.txt = 20
