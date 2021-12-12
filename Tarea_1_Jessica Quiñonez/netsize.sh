7ae9b15cae397048eb0ca7acc154fae619c6d804
1727jesy <95721069+1727jesy@users.noreply.github.com>
2021-12-11 21:01:39 -0500

    comando de filas y columnas

--- Tarea_1_Jessica Quiñonez/netsize.sh
+++ Tarea_1_Jessica Quiñonez/netsize.sh
@@ -4,3 +4,6 @@ el directorio
 CSB/unix/data/Saavedra2013
 -ls enlista los directorios y archivos
 - Seleccionamos  n10.txt 
+- Ingresamos el codigo
+echo "Numero de filas:" ; wc -l n10.txt ; echo "Numero de columnas:" ; head -n 1 n10.txt | tr -d ' ' | tr -d '\n' | wc -c
+#comando
