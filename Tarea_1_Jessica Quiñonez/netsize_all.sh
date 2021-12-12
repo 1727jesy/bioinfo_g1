d01d019b71eabe2eccc03cb8f87f616634d2fdb4
1727jesy <95721069+1727jesy@users.noreply.github.com>
2021-12-11 22:27:31 -0500

    Update netsize_all.sh
    comando utilizados para ingresar al directorio

--- Tarea_1_Jessica Quiñonez/netsize_all.sh
+++ Tarea_1_Jessica Quiñonez/netsize_all.sh
@@ -1,3 +1,13 @@
 #  Tarea del ejercicio 1.10.3 parte 2
 Ingresamos al directorio
 OneDrive/CSB-master/unix/data/Saavedra2013
+cd One drive
+ls
+cd CSB-master
+ls
+cd unix
+ls
+cd data
+ls
+cd Saavedra2013
+ls
